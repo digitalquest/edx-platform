e4f27eabf6a69b3f57c7a3700d1ae8543681eb8e
Zia Fazal <zia.fazal@arbisoft.com>
2015-07-22 16:13:21 +0500

    CertificateTemplate model
    further updates
    
    added certificate template asset model
    
    added django admin configs and migration
    
    addressed quality violations

--- lms/djangoapps/certificates/admin.py
+++ lms/djangoapps/certificates/admin.py
@@ -4,10 +4,30 @@ django admin pages for certificates models
 from django.contrib import admin
 from config_models.admin import ConfigurationModelAdmin
 from certificates.models import (
-    CertificateGenerationConfiguration, CertificateHtmlViewConfiguration, BadgeImageConfiguration
+    CertificateGenerationConfiguration,
+    CertificateHtmlViewConfiguration,
+    BadgeImageConfiguration,
+    CertificateTemplate,
+    CertificateTemplateAsset,
 )
+
+
+class CertificateTemplateAdmin(admin.ModelAdmin):
+    """
+    Django admin customizations for CertificateTemplate model
+    """
+    list_display = ('name', 'description', 'organization_id', 'course_key', 'mode', 'is_active')
+
+
+class CertificateTemplateAssetAdmin(admin.ModelAdmin):
+    """
+    Django admin customizations for CertificateTemplateAsset model
+    """
+    list_display = ('description', '__unicode__')
 
 
 admin.site.register(CertificateGenerationConfiguration)
 admin.site.register(CertificateHtmlViewConfiguration, ConfigurationModelAdmin)
 admin.site.register(BadgeImageConfiguration)
+admin.site.register(CertificateTemplate, CertificateTemplateAdmin)
+admin.site.register(CertificateTemplateAsset, CertificateTemplateAssetAdmin)
