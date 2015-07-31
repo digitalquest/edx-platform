76b9ff94ef5fc055f2241722458cd0afd9354351
Awais <awais.qureshi@arbisoft.com>
2015-07-31 17:49:58 +0500

    Basic layout of partition 'VerificationPartitionScheme'. ECOM-1977

--- openedx/core/djangoapps/credit/partition_schemes.py
+++ openedx/core/djangoapps/credit/partition_schemes.py
@@ -44,14 +44,55 @@ class VerificationPartitionScheme(object):
         return 'verification:{0}'.format(xblock_location_id)
 
     def _is_enrolled_in_verified_mode(self, user, course_key):
+        """Returns the Boolean value if given user for the given course is enrolled in
+        verified modes.
+        Arguments:
+            user(User): user object
+            course_id(CourseKey): CourseKey
+
+        Returns:
+            Boolean
+        """
         enrollment_mode, __ = CourseEnrollment.enrollment_mode_for_user(user, course_key)
         return enrollment_mode in CourseMode.VERIFIED_MODES
 
     def _was_denied_at_any_checkpoint(self, user, course_key):
-        pass
+        """Returns the Boolean value if given user with given course was denied for any
+        incourse verification checkpoint.
+        Arguments:
+            user(User): user object
+            course_id(CourseKey): CourseKey
+
+        Returns:
+            Boolean
+        """
+        return VerificationStatus.objects.filter(
+                user=user,
+                checkpoint__course_id=course_key,
+                status='denied'
+        ).exists()
 
     def _has_skipped_any_checkpoint(self, user, course_key):
+        """Check existence of a user's skipped re-verification attempt for a
+        specific course.
+
+        Arguments:
+            user(User): user object
+            course_id(CourseKey): CourseKey
+
+        Returns:
+            Boolean
+        """
         return SkippedReverification.check_user_skipped_reverification_exists(user, course_key)
 
     def _has_completed_checkpoint(self, user, course_key, checkpoint):
+        """Get the user's latest status at the checkpoint.
+        Arguments:
+            user (User): The user whose status we are retrieving.
+            course_key (CourseKey): The identifier for the course.
+            checkpoint (UsageKey): The location of the checkpoint in the course.
+
+        Returns:
+            unicode or None
+        """
         return VerificationStatus.get_user_status_at_checkpoint(user, course_key, checkpoint)
