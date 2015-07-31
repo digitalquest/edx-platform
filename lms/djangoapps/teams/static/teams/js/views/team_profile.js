068a215b790890e3220783217aa58eb8fa79e875
muzaffaryousaf <muzaffar.yousaf@arbisoft.com>
2015-07-31 18:36:48 +0500

    Adding initial design for team profile.
    TNL-1906

--- lms/djangoapps/teams/static/teams/js/views/team_profile.js
+++ lms/djangoapps/teams/static/teams/js/views/team_profile.js
@@ -4,8 +4,10 @@
 ;(function (define) {
     'use strict';
     define(['backbone', 'underscore', 'gettext', 'teams/js/views/team_discussion',
-            'text!teams/templates/team-profile.underscore'],
-        function (Backbone, _, gettext, TeamDiscussionView, team_template) {
+            'text!teams/templates/team-profile.underscore',
+            'text!teams/templates/team-member.underscore'
+        ],
+        function (Backbone, _, gettext, TeamDiscussionView, team_template, teamMemberTemplate) {
             var TeamProfileView = Backbone.View.extend({
                 initialize: function (options) {
                     this.courseID = options.courseID;
@@ -19,14 +21,41 @@
                 render: function () {
                     this.$el.html(_.template(team_template, {
                         courseID: this.courseID,
-                        discussionTopicID: this.discussionTopicID
+                        discussionTopicID: this.discussionTopicID,
+
                     }));
                     this.discussionView = new TeamDiscussionView({
                         el: this.$('.discussion-module')
                     });
                     this.discussionView.render();
+
+                    this.renderTeamMembers();
                     return this;
+                },
+
+                renderTeamMembers: function() {
+                    var view = this;
+                    _.each(this.model.get('membership'), function(data) {
+                        $.ajax({
+                            type: 'GET',
+                            url: data.user.url,
+                            success: function (data) {
+                                if (data.profile_image) {
+                                    view.$('.members-info').append(_.template(teamMemberTemplate, {
+                                        imageUrl: data.profile_image.image_url_medium,
+                                        username: data.username,
+                                        memberProfileUrl: '/u/' + data.username
+                                    }));
+                                }
+                            },
+                            error: function () {
+                                debugger
+                            }
+                        });
+
+                    });
                 }
+
             });
 
             return TeamProfileView;
